2fbc70e718fefa6b53eb63834ee2b57c120074ba
Atabek Jumaniyazov <jumaniyazovatabek6@gmail.com>
2024-07-04 09:36:08 +0500

    INFRA: Upload project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿System.Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+string greeting = $"Hello, {name}";
+System.Console.WriteLine(greeting);
+System.Console.Write("Enter you age: ");
+string ageAsString = Console.ReadLine();
+System.Console.WriteLine("Converting...");
+int age = Convert.ToInt32(ageAsString);
+System.Console.WriteLine($"Successfully converted! {age}");
+int keschasAge = 3;
+int ageDifference = age - keschasAge;
+
+System.Console.WriteLine($"The difference between your and Kescha's age is {ageDifference}");
+
+if (age > keschasAge)
+{
+    System.Console.WriteLine("Yor are older!");
+}
+else if(ageDifference == keschasAge)
+{
+    System.Console.WriteLine("Your are equal");
+}
+else
+{
+    System.Console.WriteLine("Your are younger!");
+}
